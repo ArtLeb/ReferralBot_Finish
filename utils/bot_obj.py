bc4093951372019a904989ba733bbf1009240fb6
ArtLeb <ApostolVagner@yandex.ru>
2025-08-17 19:36:03 +0300

    123

--- utils/bot_obj.py
+++ utils/bot_obj.py
@@ -1,21 +1,22 @@
 from aiogram import Bot, Dispatcher
 from aiogram.enums import ParseMode
-from aiogram.client.default import DefaultBotProperties
 from aiogram.fsm.storage.redis import RedisStorage, DefaultKeyBuilder
 from redis.asyncio.client import Redis
 from utils.config import config
 
-# Создание бота
+# Создание бота (совместимая версия)
 bot = Bot(
     token=config.BOT_TG_TOKEN,
-    default=DefaultBotProperties(parse_mode=ParseMode.HTML)
+    parse_mode=ParseMode.HTML
 )
 
 # Настройка Redis
 redis = Redis(
     host=config.REDIS_HOST,
+    port=config.REDIS_PORT,  # Добавьте порт, если используется
     username=config.REDIS_USERNAME,
     password=config.REDIS_PASSWORD,
+    db=config.REDIS_DB,  # Добавьте номер базы данных, если используется
     auto_close_connection_pool=True
 )
 
